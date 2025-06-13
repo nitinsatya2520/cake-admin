a3510bf04e4eb12fb52b5c7f854cc8209256a8e4
nitinsatya <nitinsatya656gmail.com>
2025-06-14 01:13:18 +0530

    first commit

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://cask-server.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
